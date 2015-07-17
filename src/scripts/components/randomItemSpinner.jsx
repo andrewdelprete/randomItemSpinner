6b26dd4d354670b89405bd9d083c9bc03bd26b30
Andrew Del Prete <andrew@musicbed.com>
2015-07-16 20:00:01 -0800

    Refactored this thing to be more readable hoping this will make @erikthedev_ happier and still like me.

--- src/scripts/components/randomItemSpinner.jsx
+++ src/scripts/components/randomItemSpinner.jsx
@@ -4,21 +4,62 @@ import React from 'react';
  * RandomItemSpinner Component
  */
 class RandomItemSpinner extends React.Component {
-    constructor(props) {
-        super(props);
+    componentWillMount() {
+        this.start(this.props.options.delay, this.buildPool());
     }
 
     /**
-     * Run after component mounts the donkey
+     * Spinner loops through an array with a specified delay and renders a react component
+     * @param  { Int } delay
+     * @param  { Array } items
      */
-    componentDidMount() {
-        this.init(this.props.options.delay);
+    start(delay, items) {
+
+        /**
+         * Our generator
+         */
+        function* generator() {
+            let index = 0;
+            for (let item of items) {
+                item.currentStep = index++;
+                setTimeout(onChange.bind(this), this.delayAlgorithm(item.currentStep, this.props.options.iterations, this.props.options.delay));
+                yield item;
+            }
+        }
+
+        /**
+         * Instantiate generator
+         */
+        var it = generator.call(this);
+
+        /**
+         * onChange iterator
+         */
+        var onChange = () => {
+            let item = it.next();
+
+            if (!item.value) {
+                this.setState({ end: true });
+                return;
+            }
+
+            this.playClick();
+            this.setState({ end: false, randomItem: item });
+        };
+
+        onChange();
     }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.reInit) {
-            this.init();
-        }
+    /**
+     * Build a randomize array of items to spin through
+     * @return { Array }
+     */
+    buildPool() {
+        let pool = this.iterationsSpinnerList(this.props.options.iterations / this.props.items.length, this.props.items);
+            pool = this.randomizeSpinnerArray(pool);
+            pool = this.dedupeSiblings(pool);
+
+        return pool;
     }
 
     /**
@@ -36,11 +77,11 @@ class RandomItemSpinner extends React.Component {
 
     /**
      * Randomize an Array
-     * @param  { Array } spinnerArray
+     * @param  { Array } items
      * @return { Array }
      */
-    randomizeSpinnerArray(spinnerArray) {
-        return spinnerArray.sort(this.randOrd);
+    randomizeSpinnerArray(items) {
+        return items.sort(this.randOrd);
     }
 
     /**
@@ -51,61 +92,17 @@ class RandomItemSpinner extends React.Component {
     }
 
     /**
-     * Spinner loops through an array with a specified delay and renders a react component
-     * @param  { Int } delay
-     * @param  { Array } spinnerArray
-     */
-    spinner(delay, spinnerArray) {
-
-        /**
-         * Our generator
-         */
-        function* generator() {
-            let index = 0;
-            for (let item of spinnerArray) {
-                item.currentStep = index++;
-                setTimeout(onChange.bind(this, this.props.onChangeCallback, this.props.onChangeEndCallback), this.delayAlgorithm(item.currentStep, this.props.options.iterations, this.props.options.delay));
-                yield item;
-            }
-        }
-
-        /**
-         * Instantiate generator
-         */
-        var it = generator.call(this);
-
-        /**
-         * onChange iterator
-         */
-        var onChange = (onChangeCallback, onChangeEndCallback) => {
-            let item = it.next();
-
-            if (item.value) {
-                if (onChangeCallback) {
-                    onChangeCallback.call(this);
-                }
-
-                React.render(<RandomItemSpinner element={ this.props.element } items={ this.props.items } onChangeCallback={ this.props.onChangeCallback } onChangeEndCallback={ this.props.onChangeEndCallback } options={ this.props.options } randomItem={ item } renderJSX={ this.props.renderJSX } />, this.props.element);
-            } else {
-                onChangeEndCallback();
-            }
-        };
-
-        onChange();
-    }
-
-    /**
      * Clones an array by 'n' amount of times
      * @param  { Int } multiplier
-     * @param  { Array } spinnerList
+     * @param  { Array } items
      * @return { Array }
      */
-    iterationsSpinnerList(multiplier, spinnerList) {
+    iterationsSpinnerList(multiplier, items) {
         let myArray = [];
 
-        // Multiply the spinnerList Array by 'n' amount times
+        // Multiply the items Array by 'n' amount times
         for (let x = 0; x <= multiplier; x++) {
-            myArray.push(spinnerList);
+            myArray.push(items);
         }
 
         // Flatten
@@ -116,11 +113,11 @@ class RandomItemSpinner extends React.Component {
 
     /**
      * Removes duplicate sibblings that are adjacent to one another in an Array
-     * @param  { Array } spinnerList
+     * @param  { Array } items
      * @return { Array }
      */
-    dedupeSiblings(spinnerList) {
-        return spinnerList.filter((value, index, array) => {
+    dedupeSiblings(items) {
+        return items.filter((value, index, array) => {
             if (value !== array[index - 1]) {
                 return value;
             }
@@ -128,26 +125,34 @@ class RandomItemSpinner extends React.Component {
     }
 
     /**
-     * Initialize - Render and start spinner
-     * @param  { Int } delay
-     *
-     * @TODO - Clean this up to chain somehow
+     * Event Handler to respin
      */
-    init() {
-        let spinnerList = this.iterationsSpinnerList(this.props.options.iterations / this.props.items.length, this.props.items);
-            spinnerList = this.randomizeSpinnerArray(spinnerList);
-            spinnerList = this.dedupeSiblings(spinnerList);
-
-
-        this.spinner(this.props.options.delay, spinnerList);
+    spinAgainHandler() {
+        this.start(this.props.options.delay, this.buildPool());
     }
+
+    /**
+     * Play Click Sound
+     */
+     playClick() {
+         var isSafari = (navigator.userAgent.indexOf('Safari') !== -1 && navigator.userAgent.indexOf('Chrome') === -1);
+         if (!isSafari) {
+             let audio = new Audio('mp3s/click.mp3');
+             audio.play();
+         }
+     }
 
     /**
      * Render bender
      * @return { JSX }
      */
     render() {
-        return this.props.renderJSX();
+        return (
+            <div className="RandomItemSpinner">
+                <RandomItem item={ this.state.randomItem.value } />
+                <SpinAgainButton disabled={ !this.state.end } spinAgainHandler={ this.spinAgainHandler.bind(this) } />
+            </div>
+        );
     }
 }
 
@@ -156,19 +161,59 @@ RandomItemSpinner.defaultProps = {
         delay: 120,
         iterations: 60
     },
-    reInit: false,
-    randomItem: { value: { name: '', img: '', steps: 0 } }
+    items: []
 };
 
 RandomItemSpinner.displayName = 'RandomItemSpinner';
 RandomItemSpinner.propTypes = {
-    element: React.PropTypes.object.isRequired,
     items: React.PropTypes.array.isRequired,
-    onChangeCallback: React.PropTypes.func,
-    onChangeEndCallback: React.PropTypes.func,
-    options: React.PropTypes.object,
-    randomItem: React.PropTypes.object,
-    reInit: React.PropTypes.func,
-    renderJSX: React.PropTypes.func.isRequired
+    options: React.PropTypes.object
 };
+
+/**
+ * RandomItem Component
+ */
+class RandomItem extends React.Component {
+    render() {
+        return (
+            <div className="RandomItem">
+                <img className="RandomItem__img" src={ this.props.item.img } />
+            </div>
+        );
+    }
+}
+
+RandomItem.defaultProps = {
+    item: {
+        name: null,
+        img: null
+    }
+};
+
+RandomItem.displayName = 'RandomItem';
+RandomItem.propTypes = {
+    item: React.PropTypes.object.isRequired
+};
+
+/**
+ * SpinAgainButton Component
+ */
+class SpinAgainButton extends React.Component {
+    render() {
+        let disabled = this.props.disabled ? 'disabled' : '';
+
+        return (
+            <div className="SpinAgain">
+                <button className="SpinAgain__button" disabled={ disabled } onClick={ this.props.spinAgainHandler }>&#9658;</button>
+            </div>
+        );
+    }
+}
+
+SpinAgainButton.displayName = 'SpinAgainButton';
+SpinAgainButton.propTypes = {
+    disabled: React.PropTypes.bool.isRequired,
+    spinAgainHandler: React.PropTypes.func
+};
+
 export default { RandomItemSpinner };
